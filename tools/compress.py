7ea29d65f8941c1d92bf5505b74c6fccd10f9b58
Roget Kou <roget@rkou.uk>
2021-08-24 12:59:28 +0100

    adding compression

--- tools/compress.py
+++ tools/compress.py
@@ -1,13 +1,25 @@
 #!/usr/bin/env python
 
 import pandas as pd 
+import sys, yaml
 
 from tkinter import Tk     # from tkinter import Tk for Python 3.x
-from tkinter.filedialog import askopenfilename
+from tkinter.filedialog import askdirectory, askopenfilename
+
+with open('compress_config.yml', 'r') as f:
+        compress_config = yaml.full_load(f)
+
+if len(sys.argv) > 1 and sys.argv[1] == 'config':
+# if sys.argv > 1 and sys.argv[1] == 'config':
+     output_dir = askdirectory() + '/'
+     print(output_dir)
 
 Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
 filename = askopenfilename() # show an "Open" dialog box and return the path to the selected file
+filename = filename.split('/')[-1]
 print(filename)
 
-input = pd.read_csv(filename)
-input.to_csv(filename + '.gz', compression='gzip')
+output = output_dir + filename + '.gz'
+print(output)
+# input = pd.read_csv(filename)
+# input.to_csv(filename + '.gz', compression='gzip')
--- tools/uncompress.py
+++ tools/uncompress.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python
 
 import pandas as pd
+import os
 
 from tkinter import Tk     # from tkinter import Tk for Python 3.x
 from tkinter.filedialog import askopenfilename
 
 Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
 filename = askopenfilename() # show an "Open" dialog box and return the path to the selected file
-print(filename)
+
+new = os.path.splitext(filename)[0]
+
+print(new)
+input = pd.read_csv(filename, compression='gzip')
+input.to_csv(new)
