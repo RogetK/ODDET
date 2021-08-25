d31fefe5bb0c9be825b7a65cc3dd81da647cbd07
Roget Kou <roget@rkou.uk>
2021-08-25 15:43:26 +0100

    updated compression with multicore compression for multiple files

--- tools/compress.py
+++ tools/compress.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python
 
 import pandas as pd 
-import sys, yaml
+import sys, yaml, multiprocessing
 
 from tkinter import Tk     # from tkinter import Tk for Python 3.x
 from tkinter.filedialog import askdirectory, askopenfilenames
 
 Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
+
+
+with open('compress_config.yml', 'r') as f:
+        compress_config = yaml.full_load(f)
 
 if len(sys.argv) > 1 and sys.argv[1] == 'config':
      output_dir = askdirectory() + '/'
@@ -17,20 +21,37 @@ if len(sys.argv) > 1 and sys.argv[1] == 'config':
              exit()
 
 
-files = askopenfilenames() # show an "Open" dialog box and return the path to the selected file
-file_list = list(files)
-# print(file_list)
 
-print("Compressing the following: ")
-for fp in file_list:
+def worker(fp):
         filename = fp.split('/')[-1]
         print(filename)
-
-        with open('compress_config.yml', 'r') as f:
-                compress_config = yaml.full_load(f)
 
         output_name = compress_config["output_dir"] + filename + '.gz'
         print(output_name)
         output_file = pd.read_csv(fp)
         output_file.to_csv(output_name, compression='gzip')
 
+
+
+def main():
+        files = askopenfilenames() # show an "Open" dialog box and return the path to the selected file
+        file_list = list(files)
+        print("Compressing the following files: ")
+
+        for fp in file_list:
+                jobs = []
+                p = multiprocessing.Process(target=worker, args=(fp,))
+                jobs.append(p)
+                p.start()
+
+#         filename = fp.split('/')[-1]
+#         print(filename)
+
+#         output_name = compress_config["output_dir"] + filename + '.gz'
+#         print(output_name)
+#         output_file = pd.read_csv(fp)
+#         output_file.to_csv(output_name, compression='gzip')
+
+
+if __name__ == "__main__":
+        main()
